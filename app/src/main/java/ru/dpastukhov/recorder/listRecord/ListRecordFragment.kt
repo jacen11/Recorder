ee6cbf2fa5e46cc8bfd52b61e5a5ce108102713f
jacen <jacen19931@gmail.com>
2020-04-03 21:53:55 +0300

    list record

---
+++ app/src/main/java/ru/dpastukhov/recorder/listRecord/ListRecordFragment.kt
@@ -0,0 +1,24 @@
+package ru.dpastukhov.recorder.listRecord
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import ru.dpastukhov.recorder.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ListRecordFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_record, container, false)
+    }
+
+}
